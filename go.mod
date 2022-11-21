98d88f738668da8c8dd345d58949c2900d5032c0
Nrect <boss.startov@bk.ru>
2022-11-21 17:34:08 +0200

    validate formula

--- go.mod
+++ go.mod
@@ -2,13 +2,15 @@ module testProject
 
 go 1.19
 
-require github.com/stretchr/testify v1.8.1
+require (
+	github.com/Knetic/govaluate v3.0.0+incompatible
+	github.com/samber/lo v1.35.0
+	github.com/stretchr/testify v1.8.1
+)
 
 require (
-	github.com/Knetic/govaluate v3.0.0+incompatible // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/samber/lo v1.35.0 // indirect
 	golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -2,33 +2,18 @@ package main
 
 import (
 	"fmt"
-	"github.com/samber/lo"
-	"strconv"
-	"strings"
 	"testProject/packages/calculateFromFormula"
 )
 
 func main() {
 	testStr := "555 * 111 + 5"
-	convertTestStr := strings.Split(testStr, " ")
-
-	for _, character := range convertTestStr {
-		if _, err := strconv.Atoi(character); err == nil {
-			continue
-		}
-		validateCharacter(character)
-	}
 
 	repo := calculateFromFormula.NewMockDbResponse()
 	result, err := calculateFromFormula.CalculateFromFormula(testStr, repo)
 	if err != nil {
 		fmt.Println(err)
+	} else {
+		fmt.Println(result)
+
 	}
-
-	fmt.Println(result)
-}
-
-func validateCharacter(character string) {
-	isValid := lo.Contains[string](calculateFromFormula.GetAvailableFormulaCharacters(), character)
-	fmt.Println(isValid)
 }
