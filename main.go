293e580b8211d2d3e1cc1f1fc8fd27d0743fcf52
Nrect <boss.startov@bk.ru>
2022-11-24 23:41:15 +0200

    clone some libs

--- main.go
+++ main.go
@@ -2,17 +2,9 @@ package main
 
 import (
 	"fmt"
-	"testProject/packages/calculateFromFormula"
 )
 
 func main() {
-	detailsIncome := "DetailsIncome + CommonTenantsIncome + EarnAmount"
-
-	repo := calculateFromFormula.NewMockDbResponse()
-	result, err := calculateFromFormula.CalculateFromFormula(detailsIncome, repo)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println(result)
-	}
+	fmt.Println("Hi")
+	fmt.Printf("Hahaha")
 }
