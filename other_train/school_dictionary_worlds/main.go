3706124a639831527e4858af3c2ac04f9d3956a2
Nrect <52971566+Nrect@users.noreply.github.com>
2023-03-08 23:17:59 +0200

    fix loop bug

--- other_train/school_dictionary_worlds/main.go
+++ other_train/school_dictionary_worlds/main.go
@@ -7,7 +7,7 @@ import (
 	"unicode/utf8"
 )
 
-type elementBody struct {
+type MatrixBody struct {
 	Value  string // Буква
 	Col    int32  // Номер колонки
 	Row    string // Номер строки
@@ -23,11 +23,11 @@ func main() {
 	cols := []string{"1", "2", "3", "4"}
 	rows := []string{"А", "Б", "В", "Г", "Д", "Е", "Ж", "З"}
 
-	matrixExercise := make(map[string]*elementBody)
+	matrixExercise := make(map[string]*MatrixBody)
 
 	for i, w := 0, 0; i < len(clearString); i += w {
 		runeValue, width := utf8.DecodeRuneInString(clearString[i:])
-		//fmt.Printf("%s \n", string(runeValue))
+
 		convertedValue := string(runeValue)
 
 		cypher := GenerateCypher(rows, cols)
@@ -35,29 +35,28 @@ func main() {
 		_, ok := uniqSymbols[convertedValue]
 		if !ok {
 			uniqSymbols[convertedValue] = convertedValue
-		}
 
-		_, ok = matrixExercise[cypher]
-		if !ok {
-			matrixExercise[cypher] = nil
-		} else {
-			// Очень долго ищет пару
-			//isOk := 0
-			//for isOk < 1 {
-			//	newCypher := GenerateCypher(rows, cols)
-			//	_, ok = matrixExercise[newCypher]
-			//	if !ok {
-			//		matrixExercise[newCypher] = nil
-			//		isOk = 1
-			//	}
-			//}
+			_, ok = matrixExercise[cypher]
+			if !ok {
+				matrixExercise[cypher] = nil
+			} else {
+				isOk := 0
+				for isOk < 1 {
+					newCypher := GenerateCypher(rows, cols)
+					_, ok = matrixExercise[newCypher]
+					if !ok {
+						matrixExercise[newCypher] = nil
+						isOk = 1
+					}
+				}
+			}
 		}
 
 		w = width
 	}
 
 	fmt.Println(len(uniqSymbols))
-	fmt.Println(matrixExercise)
+	fmt.Println(len(matrixExercise))
 
 }
 
@@ -69,6 +68,7 @@ func GenerateCypher(rows, cols []string) string {
 
 func GetDictionaryWords() ([]string, string) {
 	template := "весело;воробей;ворона;девочка;дежурный;деревня;заяц;карандаш;класс;корова;лисица;мальчик;машина;медведь;молоко"
+	//template := "молоко"
 
 	template = strings.TrimSpace(template)
 
